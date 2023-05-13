4bb63ca1ef7a10e1c8175d4422fa8ff49c13df43
Jose <sv44834543@idat.edu.pe>
2023-05-13 10:23:03 -0500

    actualizando

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
-var n1 = prompt("Escribe un número");
+//1.Solicitar al usuario un número, y determinar si es divisible entre dos o no.
+//Mostrando al usuario un mensaje de “x número es divisible entre 2” o “x número no
+//es divisible entre 2”.
+
+let n1 = 2;
 if (n1 % 2 === 0) {
-document.write("Es divisible por 2");
+console.log("Es divisible por 2");
 } else {
-document.write("No es divisible por 2");
+console.log("No es divisible por 2");
 }
+//2. Crear un programa que determine si un número introducido en un Prompt es par
+//o no, la respuesta será mostrada en un Alert.
+
+let numero = prompt("Introduce un número entero");
+let resultado=parImpar(numero);
+alert("El número "+numero+" es "+resultado);
+function parImpar(numero) {
+    if(numero % 2 == 0) {
+      return "par";
+    }
+    else {
+      return "impar";
+    }
+  }
+  //3.Solicitar al usuario que ingrese dos números y mostrar cuál de los dos es menor.
+  //No considerar el caso en que ambos números son iguales.
+  let a =5;
+  let b =9;
+  if(a===b)
+  console.log("son iguales");
+  if(a>b)
+  console.log("el numero mayor es : "+a);
+  if(b>a)
+  console.log("el numero mayor es : "+b);
+
+
+
+
+
 
